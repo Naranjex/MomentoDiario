bdef45ea4fe987f8c1f536662a6f926d12110985
Rafael Naranjo Jajino <83287782+Naranjex@users.noreply.github.com>
2022-11-02 18:41:03 -0300

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -11,17 +11,17 @@ function carregar (){
 
     if (hora >= 0 && hora < 12) {
     //Buenos días
-    foto.src = "./fotos/imgmanha.jpg"
+    foto.src = "fotos/imgmanha.jpg"
     document.body.style.background = '#66d9ff'
     mensagem.innerHTML += 'O Sol iluminará um <strong>bom dia!</strong>'
     } else if (hora >= 12 && hora <=18) {
     //Buenas tardes
-    foto.src = "./fotos/imgtarde.jpg"
+    foto.src = "fotos/imgtarde.jpg"
     document.body.style.background = '#ffb366'
     mensagem.innerHTML += ' Uma <strong>boa tarde</strong> lhe aguarda!'
     } else {
     // Buenas noches
-    foto.src = "./fotos/imgnoite.jpg"
+    foto.src = "fotos/imgnoite.jpg"
     document.body.style.background = '#001133'
     mensagem.innerHTML += 'Que as estrelas lhe concedam uma <strong>boa noite!</strong>'
     }
